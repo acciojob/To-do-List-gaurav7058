55dac10624b9ab9584c5613a5c747c8b54f873e7
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-10-21 15:27:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
+let arr=[]
+function handler(){
+    const inputValue=document.getElementById("newTodoInput").value;
+    const box=document.getElementById("todoList");
 
+    arr.push(inputValue)
+    box.innerHTML="";
+    arr.map((item)=>{
+        box.innerHTML+= `<li>${item}</li>`
+        document.getElementById("newTodoInput").value=""
+    })
+}
